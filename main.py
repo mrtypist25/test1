569bdf1d662d4e20e835e3497d89a889b6a8d695
Alejandro <dl491945@gmail.com>
2025-08-09 10:01:54 -0600

    function updated

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-print("Hello world")
-print("This is my first program")
-
-text = "Goog monrning, have a great day"
-for x in range(0,len(text)):
-	print(x,text[x])
-def greting():
-	print("Hello world")
-def simple_greeting():
-	print("another greeting")
+import os
+if os.path.exists(".gitignore"):
+	print("Yes")
+else:
+	print("No")
